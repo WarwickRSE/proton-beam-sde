870b674e9f24b5b26d64044a083bb6eef85cf80b
Heather Ratcliffe <h.ratcliffe@warwick.ac.uk>
2026-06-25 15:03:39 +0100

    Another function

--- Fortran/protonEffects.f90
+++ Fortran/protonEffects.f90
@@ -8,6 +8,7 @@ MODULE protonEffects
  
     SAVE
     REAL(KIND=REAL64), PARAMETER :: mecsq = 0.511_REAL64, mpcsq = 938.346_REAL64
+    REAL(KIND=REAL64), PARAMETER :: log_avogadro = LOG(6.0) + 23.0 * LOG(10.0), log_barns_to_cmsq = -24.0 * LOG(10.0)
     
     CONTAINS
 
@@ -34,12 +35,26 @@ MODULE protonEffects
 
     END FUNCTION
 
+    PURE FUNCTION nonelastic_rate(material, energy) RESULT(rate)
+      TYPE(cp_material), INTENT(IN) :: material
+      REAL(KIND=REAL64), INTENT(IN) :: energy
+      REAL(KIND=REAL64) :: rate, a
+      
+      INTEGER :: i
+
+      a = SUM(material%nucs%massFraction * material%nucs%A)
+      rate = 0.0_REAL64
+      DO i = 1, material%no_nucs
+        rate = rate + material%nucs(i)%massFraction * &
+          evaluate(NE_crossSections(material%nucs(i)%xsec_ind), energy)
+      END DO
+      rate = rate * EXP(LOG(material%density) + log_avogadro - LOG(a) + log_barns_to_cmsq) ! rate per cm
+    END FUNCTION
 
     PURE FUNCTION rutherford_and_elastic_rate(material, energy) RESULT(rate)
       TYPE(cp_material), INTENT(IN) :: material
       REAL(KIND=REAL64), INTENT(IN) :: energy
       REAL(KIND=REAL64) :: rate, a
-      REAL(KIND=REAL64), PARAMETER :: log_avogadro = LOG(6.0) + 23.0 * LOG(10.0), log_barns_to_cmsq = -24.0 * LOG(10.0)
       INTEGER :: i
 
       a = SUM(material%nucs%massFraction * material%nucs%A)
