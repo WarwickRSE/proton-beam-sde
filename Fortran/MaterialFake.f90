026563019596e8e307fb6f2f581af68093093412
Heather Ratcliffe <h.ratcliffe@warwick.ac.uk>
2026-06-26 21:00:23 +0100

    Creating sample matierals

--- Fortran/MaterialFake.f90
+++ Fortran/MaterialFake.f90
@@ -18,4 +18,43 @@ MODULE MaterialFake
     REAL(KIND=REAL64) :: mee ! Mean Excitation Energy
   END TYPE
 
+
+  CONTAINS
+
+  FUNCTION createCarbon() RESULT(testMaterial)
+    TYPE(cp_material) :: testMaterial
+    testMaterial%no_nucs = 1
+    testMaterial%density = 1.0_REAL64
+    testMaterial%mee = 1.0_REAL64
+    ALLOCATE(testMaterial%nucs(1))
+    testMaterial%nucs(1)%name = 'carbon'
+    testMaterial%nucs(1)%num_dens = 1.0_REAL64
+    testMaterial%nucs(1)%Z = 6
+    testMaterial%nucs(1)%A = 12.011
+    testMaterial%nucs(1)%massFraction = 1.0
+
+  END FUNCTION
+
+  FUNCTION createWater() RESULT(testMaterial)
+    TYPE(cp_material) :: testMaterial
+    testMaterial%no_nucs = 2
+    testMaterial%density = 1.0_REAL64
+    testMaterial%mee = 1.0_REAL64
+
+    ALLOCATE(testMaterial%nucs(2))
+    testMaterial%nucs(1)%name = 'hydrogen'
+    testMaterial%nucs(1)%num_dens = 0.33
+    testMaterial%nucs(1)%Z = 1
+    testMaterial%nucs(1)%A = 1.008
+    testMaterial%nucs(1)%massFraction = 0.33
+
+    testMaterial%nucs(2)%name = 'oxygen'
+    testMaterial%nucs(2)%num_dens = 0.67
+    testMaterial%nucs(2)%Z = 8
+    testMaterial%nucs(2)%A = 15.999
+    testMaterial%nucs(2)%massFraction = 0.67
+
+  END FUNCTION
+
+
 END MODULE
