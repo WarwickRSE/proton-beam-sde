1fcafb91bfb7885650a55594bab28e0fd6483452
Heather Ratcliffe <h.ratcliffe@warwick.ac.uk>
2026-06-29 14:27:50 +0100

    Add updated script

--- Splines/reprocess.sh
+++ Splines/reprocess.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-files=($(ls *.txt))
+files=($(ls *_ne_rate.txt))
 for file in "${files[@]}" ; do
   ct=$(head -n 1 $file | wc -w)
   echo $ct > $file".bak"
+  cat $file >> $file".bak"
+  mv $file".bak" $file
+done
+
+files=($(ls *_el_ruth_cross_sec.txt))
+for file in "${files[@]}" ; do
+  ct=$(head -n 1 $file | wc -w)
+  ct2=$(head -n 3 $file | tail -n 1 | wc -w)
+  echo $ct $ct2 > $file".bak"
   cat $file >> $file".bak"
   mv $file".bak" $file
 done
